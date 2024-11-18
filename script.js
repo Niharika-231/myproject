04f4d83ada0cf77b1a7f653556822b6e75096fba
Niharika-231 <138964470+Niharika-231@users.noreply.github.com>
2024-11-18 15:17:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth Scroll Script
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
